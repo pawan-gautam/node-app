740f4da2682c6a7bddbb414906d147ef3fff9b38
pawan gautam <nickelfox@pop-os.localdomain>
2024-01-29 12:36:26 +0530

    done

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.send('Hello, Express111!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(PORT, '0.0.0.0',() => {
+  console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
